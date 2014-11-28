4e04bf60a5f5df5575a638293ad11dbe5b677e1a
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-11-28 12:11:24 +0100

    sauvegarde

---
+++ web/js/admin/zooparc/staticpage.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    function displayFormMsg(msg, target){
+
+    }
+
+    var addPageForm = $("form#add-page"),
+        addPageFormMsg  = $("#add-page-form-msg"),
+        addPageBtn = $("#add-page-btn");
+
+    addPageBtn.on("click", function(e){
+        e.preventDefault();
+
+
+        $.post().done(function(response){
+            if(response.error === false){
+
+            } else {
+
+            }
+            displayFormMsg(response.msg, addPageFormMsg);
+        }).fail(function(){
+
+        });
+    })
+});
