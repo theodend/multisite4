397295dcee82bb0d54134d33cab803c46f7c3af4
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-12-05 13:00:22 +0100

    sauvegarde

--- web/js/devs/alert_manager.ts
+++ web/js/devs/alert_manager.ts
@@ -3,25 +3,21 @@
 /// <reference path="dts/jquery.cookie.d.ts" />
 
 class AlertManager{
-
     private overlay:JQuery;
     private modal:JQuery;
     private closeBtn:JQuery;
     private options = {};
-
     static DEFAULTS = {
         overlayId: "#alert-overlay",
         modalId: "#alert-modal",
         closeBtnId: "#alert-close-btn",
         cookieKeyName: "zpb.alert"
     };
-
     constructor(options?:any){
         this.options = $.extend(true, {}, AlertManager.DEFAULTS, options||{});
         this.overlay = $(this.options["overlayId"]);
         this.modal = $(this.options["modalId"]);
         this.closeBtn = $(this.options["closeBtnId"]);
-
         if(this.checkCookie()){
             this.initEvents();
             this.open();
@@ -29,17 +25,14 @@ class AlertManager{
             this.close();
         }
     }
-
     open():void{
         this.overlay.addClass("open");
         this.modal.addClass("open");
     }
-
     checkCookie():boolean{
         return $.cookie(this.options["cookieKeyName"]) == undefined;
 
     }
-
     close():void{
         if($.cookie(this.options["cookieKeyName"]) == undefined ){
             $.cookie(this.options["cookieKeyName"], "ok");
@@ -49,10 +42,8 @@ class AlertManager{
             this.remove();
         });
     }
-
     initEvents():void{
         var self = this;
-
         this.overlay.on("click", function(e){
             e.preventDefault();
             self.close();
@@ -61,6 +52,5 @@ class AlertManager{
             e.preventDefault();
             self.close();
         });
-
     }
 }
