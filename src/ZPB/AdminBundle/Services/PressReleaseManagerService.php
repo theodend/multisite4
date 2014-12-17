de157f77d0d19ac756628584ddda411b487c295a
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-12-17 10:04:00 +0100

    sauvegarde

---
+++ src/ZPB/AdminBundle/Services/PressReleaseManagerService.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrère
+ * Date: 17/12/2014
+ * Time: 10:00
+ */
+  /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\AdminBundle\Services;
+
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class PressReleaseManagerService {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+
+
+}
